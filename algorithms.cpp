15e56b972f4ca8e68dd6d40e565190ef29a65e7b
petersn86 <133061453+petersn86@users.noreply.github.com>
2024-04-28 16:41:26 -0400

    basic ops

--- algorithms.cpp
+++ algorithms.cpp
@@ -6,23 +6,28 @@ using namespace std;
 int m, n;
 char* x;
 char* y;
+int basicOps;
 
 
 int opt(int i, int j){
     if(i == m){
         return 2 * (n  - j);
+        basicOps += 4;
     }
     else if(j == n){
         return 2 * (m - i);
+        basicOps += 5;
     }
     else{
         int penalty;
         if(x[i] == y[j]){
             penalty = 0;
+            basicOps += 1;
         }
         else{
             penalty = 1;
         }
+        basicOps += 10;
         return min({opt(i + 1, j + 1) + penalty, opt(i + 1, j) + 2, opt(i, j + 1) + 2});
     }
 
@@ -33,27 +38,34 @@ int optDP() {
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
 
     // Initialize the base cases
-    for (int i = 0; i <= m; ++i) {
+    for (int i = 0; i <= m; i++) {
         dp[i][n] = 2 * (m - i);
+        basicOps += 3;
     }
-    for (int j = 0; j <= n; ++j) {
+    for (int j = 0; j <= n; j++) {
         dp[m][j] = 2 * (n - j);
+        basicOps += 3;
     }
 
     // Bottom-up dynamic programming
-    for (int i = m - 1; i >= 0; --i) {
-        for (int j = n - 1; j >= 0; --j) {
+    for (int i = m - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
             int penalty;
+            basicOps += 2;
             if (x[i] == y[j]) {
                 penalty = 0;
+                basicOps ++;
             } else {
                 penalty = 1;
             }
+            basicOps += 10;
             dp[i][j] = min({dp[i + 1][j + 1] + penalty, dp[i + 1][j] + 2, dp[i][j + 1] + 2});
         }
+        basicOps += 2;
     }
 
     // Return the optimal alignment score
+    basicOps ++;
     return dp[0][0];
 }
 
